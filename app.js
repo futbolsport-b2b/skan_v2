12e4ff6f9f14362e2bb24325188ad5200f77b658
futbolsport-b2b <lukaszchodor@gmail.com>
2026-04-13 08:31:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzu8Co-M2J_-YloMbGnlDjF6dMqUwGBH0uYxTW3pkykLzjUdF3-cSnGRrk0KASKpPTA/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzL04PTWLIlLfWxJx1i0Dg-nBPQ_M9S8sb0uShPjblns89ies7w_77ZS6VTSvUsUXkn/exec"; 
 const IMAGE_BASE_URL = "https://b2b.futbolsport.pl/gfx-base/s_1/gfx/products/big/"; 
 
 let currentUser = null, currentOrderID = null, targetItem = null;
